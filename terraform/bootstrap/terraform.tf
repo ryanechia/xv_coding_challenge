50db689dd13c1de4edaafa291bc3e45574bba6fb
Ryane Chia <ryanechia@outlook.com>
2020-10-26 00:14:30 +0800

    init tfstate management into s3

---
+++ terraform/bootstrap/terraform.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "namespace" {
+  default = "xv-coding-challenge-ryane-c"
+}
+
+provider "aws" {
+  region  = "${var.region}"
+  version = "1.25.0"
+}
+
+provider "random" {
+  version = "1.3.1"
+}
+
+module "config-state" {
+  source              = "cloudowski/config-state/aws"
+  version             = "0.2.0"
+  region              = "${var.region}"
+  bucket_name         = "${var.namespace}-tfstate"
+  dynamodb_table_name = "${var.namespace}-tfstate"
+}
