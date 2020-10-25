55f79257a0d7bfeb26ac5839b86143a651350a4e
Ryane Chia <ryanechia@outlook.com>
2020-10-26 03:55:04 +0800

    update lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,4 +1,26 @@
+'use strict';
+
 exports.handler = (event, context, callback) => {
-    console.log('Hello, logs!');
-    callback(null, 'great success');
+    let noCacheHeaders = {
+        'cache-control': [{
+            key: 'Cache-Control',
+            value: 'no-cache'
+        }],
+        'pragma': [{
+            key: 'Pragma',
+            value: 'no-cache'
+        }],
+        'content-type': [{
+            key: 'Content-Type',
+            value: 'application/json'
+        }]
+    };
+
+    const response = {
+                    status: '200',
+                    statusDescription: 'OK',
+                    headers: noCacheHeaders,//cachedHeaders,
+                    body: 'Hello, logs!';
+                };
+    callback(null, response);
 }
